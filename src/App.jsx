52e10d9b87a3dad7d5230fde50adf3420d8510e0
bang <sqibang@gmail.com>
2017-02-28 23:45:54 +0700

    chore: 😊

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import * as React from "react";
-import { Component } from 'react';
-import './App.css';
-import logo from './logo.svg';
+import { Component } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+// antd stuff
+import { Button, message } from "antd";
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React!</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit
-                    <code>src/App.js</code>
-                    and save to reload.
-                </p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React!</h2>
+        </div>
+        <div className="App-intro">
+          To get started, edit
+          <code>src/App.js</code>
+          and save to reload.
+          <div>
+            <Button
+              type="primary"
+              onClick={() => message.success("Hi there !")}
+            >
+              I am a Button.
+            </Button>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
+// import ant style
+import "antd/dist/antd.css"
 
 
 ReactDOM.render(
